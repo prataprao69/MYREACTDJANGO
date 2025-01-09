d22baf77163898fbb3ad4771a5dffc8a0885dff1
prataprao69 <pratapraopravinkale@gmail.com>
2025-01-09 12:27:53 +0530

    superuserregistration

--- myRD/RD/admin.py
+++ myRD/RD/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from RD.models import Item
 # Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display=['id','name','description']
+
+admin.site.register(Item,ItemAdmin)
